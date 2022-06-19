71c11a73d9ebf970f83d25fa1d7530d67102966a
Jacob <jrottenberg@umass.edu>
2022-06-19 14:56:44 -0400

    Added header, search bar, and search button with event listener

---
+++ main.js
@@ -0,0 +1,7 @@
+const searchButton = document.getElementById('search');
+
+searchButton.addEventListener("click", () => {
+    const searchBar = document.getElementById('search-bar');
+    const query = searchBar.value;
+    // TODO Implement Giving Global API with search
+})
