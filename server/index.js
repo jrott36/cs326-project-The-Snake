51f22b34d3b1aea8d9783367ba0c600dd86088e4
Jacob Rottenberg <jrottenberg@umass.edu>
2022-07-12 21:59:06 -0400

    Fixed logout needing callback issue

--- server/index.js
+++ server/index.js
@@ -56,8 +56,10 @@ app.post(
     })
   );
 
-app.get('/logout', (req, res) => {
-    req.logout();
+app.get('/logout', (req, res, next) => {
+    req.logout((err) => {
+        if (err) { return next(err); }
+    });
     res.redirect('/login');
 });
 
